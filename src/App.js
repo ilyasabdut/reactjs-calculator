dc84748e1069dad16dd5104e1e476916756ad493
ilyas abduttawab <ilyasabduttawab@gmail.com>
2021-01-07 17:54:36 +0700

    finished and deployed

--- src/App.js
+++ src/App.js
@@ -1,85 +1,107 @@
-import React, {Component} from 'react'
-import {Card, Button, Form, Row, Col} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Card, Button, Form, Row, Col } from 'react-bootstrap';
 
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      input1:  0,
-      input2: 0,
-      input3: 0,
-      results: 0 
-    };
-    this.handleinput1Change = this.handleinput1Change.bind(this);
-    this.handleinput2Change = this.handleinput2Change.bind(this);
-    this.handleinput3Change = this.handleinput3Change.bind(this);
-  }
-  handleinput1Change (evt) {
-    console.log(evt.target.value);
-    this.setState({ input1: Number(evt.target.value) });
-  }
-  handleinput2Change(evt) {
-    console.log(typeof evt.target.value);
-    this.setState({ input2: Number(evt.target.value) });
-  }
-  handleinput3Change(evt) {
-    console.log(typeof evt.target.value);
-    this.setState({ input3: Number(evt.target.value) });
-  }
-
-  addAction =(event)=> {
-    let x = this.state.input1 + this.state.input2 + this.state.input3
-    this.setState({results: x })
-  }
-
-  substractAction =(event)=> {
-    let x = this.state.input1 - this.state.input2 - this.state.input3
-    this.setState({results: x })
-  }
-
-  render() {
-    return (
-    <Card>
-      <Card.Header>Featured</Card.Header>
-      <Card.Body>
-        <Form className={"container"}>
-          <Form.Group as = {Row} id="input1">
-            <Col sm={{span:4 ,offset: 2}}>
-              <Form.Control placeholder="Input1" type="number" onChange={this.handleinput1Change}/>
-              
-            </Col>
-              <Form.Check type="checkbox"/>
-          </Form.Group>
-
-          <Form.Group as = {Row} id="input2">
-            <Col sm={{span:4 ,offset: 2}}>
-              <Form.Control placeholder="Input2" type="number" onChange={this.handleinput2Change}/>
-            </Col>
-              <Form.Check type="checkbox"/>
-          </Form.Group>
-
-          <Form.Group as = {Row} id="input3">
-            <Col sm={{span:4 ,offset: 2}}>
-              <Form.Control placeholder="Input3" type="number" onChange={this.handleinput3Change}/>
-            </Col>
-              <Form.Check type="checkbox"/>
-          </Form.Group>
-          
-          <Col sm={{offset: 3}}>
-            <input type="button" onClick={this.addAction} value="+" /> {' '}
-            <input type="button" onClick={this.substractAction} value="-" /> {' '}
-            <Button>x</Button> {' '}
-            <Button>/</Button> {' '}
-          </Col>
-        </Form>
-      </Card.Body>
-      <Card.Footer className="text-muted">Results: {this.state.results}</Card.Footer>
-    </Card>
-
-
+function App() {
+    const [list, setList] = useState([
+        { value: "0", isChecked: true, placeholder: "input1" },
+        { value: "0", isChecked: true, placeholder: "input2" },
+        { value: "0", isChecked: false, placeholder: "input3" }]
     )
-  }
-}
+    //   const [count, setCount] = useState(0);
+    const [result, setResult] = useState(0);
+    const [operand, setOperand] = useState("+");
 
+    const handleInputChange = (selectedIndex, value) => {
+        setList(list.map((item, index) => {
+            if (selectedIndex === index) {
+                return { ...item, value: value }
+            } return item
+        }))
+    }
+
+    const handleCheckboxChange = (selectedIndex, value) => {
+        setList(list.map((item, index) => {
+            if (selectedIndex === index) {
+                return { ...item, isChecked: value }
+            } return item
+        }))
+    }
+
+    const handleOperandChange = (value) => {
+        setOperand(value)
+    }
+
+    const isCheckedCount = list.reduce((total, item) => {
+        return total + (item.isChecked ? 1 : 0)
+    }, 0)
+
+    useEffect(() => {
+        if (isCheckedCount === 1) {
+            alert("Error! tick 2 checkboxes or more!")
+        } else if (isCheckedCount === 0) {
+            setResult(0)
+        }
+        else {
+            switch (operand) {
+                default:
+                    setResult(
+                        list.reduce((total, item) => {
+                            return item.isChecked ? parseInt(item.value, 10) + total : total
+                        }, 0))
+                break
+                case "-":
+                    setResult(
+                        list.reduce((total, item) => {
+                            if(item.isChecked === true){
+                                if(total === 0){
+                                    return parseInt(item.value, 10)
+                                }else {
+                                    return total - parseInt(item.value, 10) 
+                                }
+                            }
+                            return total
+                        }, 0))
+                break
+                case "x":
+                    setResult(
+                        list.reduce((total, item) => {
+                            return item.isChecked ? parseInt(item.value, 10) * total : total
+                        }, 1))
+                break
+                case "/":
+                    setResult(
+                        list.reduce((total, item) => {
+                            return item.isChecked ? parseInt(item.value, 10) / total : total
+                        }, 1))
+                break
+            }
+        }
+    }, [list, isCheckedCount, operand]);
+
+    return (
+        <Card>
+            <Card.Header>Ilyas Abduttawab</Card.Header>
+            <Card.Body>
+                <Form className={"container"}>
+                    {list.map((item, index) => {
+                        return (<Form.Group as={Row} key={index}>
+                            <Col sm={{ span: 4, offset: 2 }}>
+                                <Form.Control onChange={(event) => handleInputChange(index, event.target.value)} placeholder={item.placeholder} type="number" value={item.value} disabled={!item.isChecked}/>
+                            </Col>
+                            <Form.Check type="checkbox" checked={item.isChecked} value={item.isChecked} onChange={(event) => handleCheckboxChange(index, event.target.checked)} />
+                        </Form.Group>)
+                    })}
+
+                    <Col sm={{ offset: 3 }}>
+                        <Button onClick={() => handleOperandChange("+")} variant={operand === "+" ? "success" : "primary"}>+</Button> {' '}
+                        <Button onClick={() => handleOperandChange("-")} variant={operand === "-" ? "success" : "primary"}>-</Button> {' '}
+                        <Button onClick={() => handleOperandChange("x")} variant={operand === "x" ? "success" : "primary"}>x</Button> {' '}
+                        <Button onClick={() => handleOperandChange("/")} variant={operand === "/" ? "success" : "primary"}>/</Button> {' '}
+                    </Col>
+                </Form>
+            </Card.Body>
+            <Card.Footer className="text-muted">{`Results: ${result}`}</Card.Footer>
+        </Card>
+    );
+}
 export default App
